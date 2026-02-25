d8274145e273844b00ca4bc7d631fd24643ebe37
nat <natscott@nats.solutions>
2026-02-25 08:55:00 -0800

    feat: add message bubble to App.jsx for illustration purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Register from './components/Register';
+import MessageBubble from './components/MessageBubble';
 
 function App() {
     return (
@@ -10,6 +11,11 @@ function App() {
                 <p>Explore our collection of books, genres, and more!</p>
             </main>
 	    <Register />
+			<MessageBubble
+				message='This is a test message. Explicabo et dolores consequatur voluptas assumenda optio. Quia inventore natus ipsum. Distinctio sunt provident culpa enim mollitia dicta ut. Mollitia corrupti vel est aliquam eveniet nisi maiores.'
+				alignment='left'
+				sender_src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Magnolia_grandiflora_-_flower_1.jpg/330px-Magnolia_grandiflora_-_flower_1.jpg'
+				/>
         </div>
     );
 }
