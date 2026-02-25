a839eaf3a758fa18c4b3548c8a4f2334c77d5f72
megan <davamae.md@gmail.com>
2026-02-25 08:39:24 -0800

    Navbar component created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className="main-nav">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/collections">Collections</a></li>
+                <li><a href="/genres">Genres</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
