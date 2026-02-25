8dc4814bad895866896d50835d22f0ce16d54759
megan <davamae.md@gmail.com>
2026-02-25 08:38:25 -0800

    App.jsx file created

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <h2>Welcome to the Virtual Library</h2>
+                <p>Explore our collection of books, genres, and more!</p>
+            </main>
+        </div>
+    );
+}
+
+export default App;
