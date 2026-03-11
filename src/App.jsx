5651829bd63165717dc7f52ab696aa4393db892b
techisanerd <thetechboy0000@gmail.com>
2026-03-10 18:36:05 -0700

    in class activity -- frontend work

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Header from './components/Header';
-import Register from './components/Register';
 import MessageBubble from './components/MessageBubble';
 import LogIn from './components/LogIn'; 
 
+import { useState, useEffect } from 'react';
+
 function App() {
+	const [book, setBook] = useState({});
+	  useEffect(() => {
+		  (async () => {
+		  const res = await fetch('/api/book');
+		  const data = await res.json();
+		  setBook(data);
+
+		  })();
+
+	  }, []);
     return (
         <div className="App">
             <Header />
@@ -11,13 +22,13 @@ function App() {
                 <h2>Welcome to the Virtual Library</h2>
                 <p>Explore our collection of books, genres, and more!</p>
             </main>
-	    <Register />
 			<MessageBubble
 				message='This is a test message. Explicabo et dolores consequatur voluptas assumenda optio. Quia inventore natus ipsum. Distinctio sunt provident culpa enim mollitia dicta ut. Mollitia corrupti vel est aliquam eveniet nisi maiores.'
 				alignment='left'
 				sender_src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Magnolia_grandiflora_-_flower_1.jpg/330px-Magnolia_grandiflora_-_flower_1.jpg'
 				/>
         <LogIn />
+	    <p>{book.title}</p>
         </div>
     );
 }
