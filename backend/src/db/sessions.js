33d126d7faf07ad26ae8d6a8bc04ca09d0272a64
nat <natscott@nats.solutions>
2026-03-17 11:42:45 -0700

    feat(backend): add DAL for sessions

---
+++ backend/src/db/sessions.js
@@ -0,0 +1,58 @@
+import { randomBytes } from 'node:crypto';
+import { objectId } from '#src/db/connection.js';
+
+/*
+{
+	_id: ObjectID of session
+	account_id: Account of the person who's logged-in
+	token: cryptographically secure random string acting to prove authorization
+	expires: Unix timestamp (seconds) after which the session is no longer valid
+}
+*/
+
+/**
+ * Returns a document corresponding to a session, given a session token.
+ * This function may be used as a part of validating an existing session
+ */
+export function getSessionByToken(connection, session_token) {
+	return connection
+		.collection('sessions')
+		.findOne({ token: session_token, });
+}
+
+/**
+ * Create a new session, given an account ID. Returns the session token
+ */
+export function createSession(connection, account_id) {
+	if (!process.env.SESSION_LIFESPAN_SECONDS) {
+		throw new Error('SESSION_LIFESPAN_SECONDS not specified in .env file');
+	}
+
+	const session_token = Buffer.from(randomBytes(256)).toString('hex');
+
+	connection
+		.collection('sessions')
+		.insertOne({
+			account_id: objectId(account_id),
+			token: session_token,
+			expires: Math.floor(Date.now()/1000) + process.env.SESSION_LIFESPAN_SECONDS
+		});
+	
+	return session_token;
+}
+
+export function deleteSession(connection, session_token) {
+	return connection
+		.collection('sessions')
+		.deleteOne({ token: session_token, });
+}
+
+/**
+ * Delete all of a given person's sessions. This is sort of like logging out
+ * on all devices
+ */
+export function deleteAllSessions(connection, account_id) {
+	return connection
+		.collection('sessions')
+		.deleteMany({ account_id: objectId(account_id), });
+}
