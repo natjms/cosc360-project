50cec0a64bc4a5e87a2bf3acdf2d1e41c8bf8411
ailishc8-byte <ailishc8@gmail.com>
2026-03-18 09:43:11 -0700

    pushing my login information for login lab 7

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -16,26 +16,46 @@ export default function Login(props) {
          e.preventDefault() 
          if(!email.trim() || !password.trim()) return; 
 
-         setIsSubmitting(true)
          try { 
-            await createLoginForm(content)
-            setPassword("")
-            setEmail()
-            onCreate(); 
-         } catch { 
+          const response = await fetch("http://localhost:3000/api/user", { 
+            method: "POST", 
+            headers: { 
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email, password})
+     })
 
-         } finally { 
-            setIsSubmitting(false)
-         }
+
+        if(!response.ok) { 
+          throw new Error("Error"); 
+        }
+          const data = await response.json(); 
+          console.log(data)
+          const stat = document.getElementById("response")
+
+          if(data.success) { 
+            setIsSubmitting(true);
+            console.log("Login successful")
+            navigate('/');
+          } else { 
+            console.log("Login unsuccessful")
+            stat.textContent = "Successful"
+          }
+        } catch(error) { 
+            console.log("error logging in")
+          } finally { 
+            setIsSubmitting(false); 
+          }
 
     };
+
 
     return (
         <>
         <div className = "window" onClick = {props.toggle}>
             <button className = "close" onClick = {props.toggle}>Close</button>
           <div className = "overlay" onClick = {(e) => e.stopPropagation()}>
-            <div className = "content">
+            <form className = "content" onSubmit ={handleSubmit}>
               <h2 className="title">LOG IN</h2>
                   <label htmlFor = "email">EMAIL</label> 
                   <input 
@@ -43,21 +63,22 @@ export default function Login(props) {
                     name = "email" 
                     id = "email" 
                     placeholder = "Enter an email"
-                    value={password}
-                    onChange={(e) => setEmail(e.target.value)}
-                    ></input>
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}>
+                    </input>
 
                   <label htmlFor = "Password">PASSWORD</label> 
                   <input 
-                    type ="text" 
+                    type ="password" 
                     name = "password" 
                     id = "password" 
                     placeholder = "Enter a password"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    ></input> 
-                  <button className = "submit" onClick ={handleSubmit}>Submit</button>
-              </div>
+                    onChange={(e) => setPassword(e.target.value)}>
+                    </input> 
+                  <button className = "submit" type = "submit">Submit</button>
+                  <p id = "response" style = {{marginLeft: "20px"}}></p>
+             </form>
              </div>
              <p>No Account?</p>
              <button onClick={handleSignup}>Signup</button>
@@ -65,4 +86,3 @@ export default function Login(props) {
         </>
     );
 }
-
