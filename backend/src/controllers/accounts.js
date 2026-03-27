77c4fac94a83bce8937b9e28541c8dd6e5df3ef9
nat <natscott@nats.solutions>
2026-03-27 11:32:12 -0700

    feat: move Estellas register controller into the accounts controller

--- backend/src/controllers/accounts.js
+++ backend/src/controllers/accounts.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { SL, at_least } from '#src/authentication.js';
 import { connect_db } from '#src/db/connection.js';
 
+import * as accounts from '#src/db/accounts.js';
+
 const router = express.Router();
 
 const unimplemented = (req, res) => {
@@ -13,6 +15,25 @@ router.use(connect_db);
 
 // Get all accounts as a list
 router.get('/', at_least(SL.admin), unimplemented);
+router.post('/', at_least(SL.unauthenticated), async (req, res) => {
+	try {
+		const accountId = await accounts.createAccount(req.conn, req.body);
+		res.status(201).send({ id: accountId });
+	} catch (e) {
+		if (e.issues?.length > 0) {
+			res.status(400);
+			res.send({
+				error: e.message,
+				issues: e.issues,
+			});
+			return;
+		} else {
+			console.error(e);
+			res.status(500);
+			res.send({error: 'An unknown error occured. Please try again later'});
+		}
+	}
+});
 
 router.get('/:account_id', at_least(SL.unauthenticated), unimplemented);
 router.patch('/:account_id', at_least(SL.authenticated), unimplemented);
