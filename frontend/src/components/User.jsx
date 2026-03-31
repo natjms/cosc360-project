f82cc8f8df972064d409b279a15d47c43c805887
techisanerd <thetechboy0000@gmail.com>
2026-03-31 12:02:05 -0700

    really basic user page; default.css

---
+++ frontend/src/components/User.jsx
@@ -0,0 +1,30 @@
+import Header from './Header.jsx'
+import './User.css';
+
+//query user from backend
+//if exists, do something. if not 
+
+
+
+function User(){
+	return (
+		<div>
+				<Header />
+				<div class="user-banner">
+				<img src="notsureyet"></img>
+				<h1>username</h1>
+
+				<h2>location</h2>
+				<h2>joined date</h2>
+				<h2>num books</h2>
+			</div>
+
+
+		
+
+
+
+
+		</div>);
+}
+export default User;
