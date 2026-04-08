56b4024b10865a5df2ebc04223dce2139bd84765
nat <natscott@nats.solutions>
2026-04-08 08:13:33 -0700

    feat: initial, untested implementation of conversation and notification controllers

--- backend/src/controllers/conversations.js
+++ backend/src/controllers/conversations.js
@@ -1,29 +1,126 @@
 import express from 'express';
 import { SL, at_least } from '#src/authentication.js';
-import { connect_db } from '#src/db/connection.js';
+import { connect_db, objectId } from '#src/db/connection.js';
+
+import * as accounts from '#src/db/accounts.js';
+import * as books from '#src/db/books.js';
+import * as catalog from '#src/db/catalog.js';
+import * as conversations from '#src/db/conversations.js';
+import * as messages from '#src/db/messages.js';
 
 const router = express.Router();
 
-const unimplemented = (req, res) => {
-	res.status(500);
-	res.send({ error: 'UNIMPLEMENTED' });
-}
+const populate = async conversation => {
+	conversation.initator = await accounts.getAccountById(req.conn, conversation.initiator);
+	conversation.recipient = await accounts.getAccountById(req.conn, conversation.recipient);
+	conversation.last_message = await messages.getMessageById(req.conn, conversation.last_message);
+	conversation.context = await books.getBookById(req.conn, conversation.context);
+};
 
 router.use(connect_db);
 
-router.get('/', at_least(SL.admin), unimplemented);
-
 // Get someone's list of conversations
-router.get('/:account_id', at_least(SL.authenticated), unimplemented);
+router.get('/:account_id', at_least(SL.authenticated), async (req, res) => {
+	if (req.account.username !== 'admin' && !objectId(req.params.account_id).equals(req.account._id)) {
+		res.status(401).send({ error: 'You can only see your own conversations' });
+		return;
+	}
+
+	let conversation_list = await conversations.getConversations(req.conn, req.account._id);
+
+	// cursed manual ObjectID population
+	for (const i in conversation_list) {
+		conversation_list[i] = await populate(new_conversation);
+	}
+
+	res.status(200).send(conversation_list);
+});
 
 // Start conversation with someone
-router.post('/:account_id', at_least(SL.authenticated), unimplemented);
+router.post('/:account_id', at_least(SL.authenticated), async (req, res) => {
+	const new_conversation_id =
+		await conversations.startConversation(
+			req.conn, req.account._id, req.params.account_id, req.body.book);
+
+	let new_conversation =
+		await conversations.getConversationById(req.conn, new_conversation_id);
+	
+	new_conversation = await populate(new_conversation);
+	res.status(201).send(new_conversation);
+});
 
 // Get list of messages in a particular conversation
-router.get('/:account_id/:conversation_id', at_least(SL.authenticated), unimplemented);
-router.delete('/:account_id/:conversation_id', at_least(SL.authenticated), unimplemented);
+router.get('/:account_id/:conversation_id', at_least(SL.authenticated), async (req, res) => {
+	let conversation = await conversations.getConversationById(req.conn, req.params.conversation_id);
+	if (conversation === null) {
+		res.status(404).send({error: 'Unknown conversation'});
+		return;
+	}
+
+	if (
+		!conversation.recipient.equals(objectId(req.account._id)) &&
+		!conversation.initiator.equals(objectId(req.account._id))
+	) {
+		// Lying for security reasons
+		res.status(404).send({error: 'Unknown conversation'});
+		return;
+	}
+
+	conversation = await populate(conversation);
+
+	// Add messages
+	let messages = await messages.getMessagesInConversation(req.conn, conversation._id);
+	conversation.messages = messages;
+
+	await conversations.markConversationRead(req.conn, conversation._id);
+
+	res.status(200).send(conversation);
+});
+
+router.patch('/:account_id/:conversation_id/complete', at_least(SL.authenticated), async (req, res) => {
+	let conversation = await conversations.getConversationById(req.conn, req.params.conversation_id);
+	if (conversation === null) {
+		res.status(404).send({error: 'Unknown conversation'});
+		return;
+	}
+
+	if (
+		!conversation.recipient.equals(objectId(req.account._id)) &&
+		!conversation.initiator.equals(objectId(req.account._id))
+	) {
+		// Lying for security reasons
+		res.status(404).send({error: 'Unknown conversation'});
+		return;
+	}
+
+	await conversations.markConversationComplete(req.conn, conversation._id);
+	res.status(204);
+	
+});
 
 // Send message to someone in a particular conversation
-router.post('/:account_id/:conversation_id', at_least(SL.authenticated), unimplemented);
+router.post('/:account_id/:conversation_id', at_least(SL.authenticated), async (req, res) => {
+	const message_id = await messages.sendMessage(
+		req.conn, req.params.conversation_id, req.account._id, req.body.content);
+
+	const message = await messages.getMessageById(req.conn, message_id);
+
+	res.status(201).send(message)
+});
+
+router.use(async (err, req, res, next) => {
+	if (res.headersSent) {
+		next(err);
+	}
+
+	if (err instanceof DBError) {
+		res.status(400).send(err.sendable());
+		return;
+	} else {
+		console.error(err);
+		res.status(500);
+		res.send({error: 'An unknown error occured. Please try again later'});
+	}
+});
 
 export default router;
--- backend/src/controllers/notifications.js
+++ backend/src/controllers/notifications.js
@@ -9,10 +9,66 @@ const unimplemented = (req, res) => {
 	res.send({ error: 'UNIMPLEMENTED' });
 }
 
+const its_mine = (req, res, next) => {
+	if (req.account.username !== 'admin' && !objectId(req.params.account_id).equals(req.account._id)) {
+		res.status(401).send({ error: 'You can only read your own notifications' });
+		return;
+	}
+
+	next();
+};
+
 router.use(connect_db);
 
-router.get('/', at_least(SL.admin), unimplemented);
-router.get('/:account_id', at_least(SL.authenticated));
-router.delete('/:account_id/:notification_id', at_least(SL.authenticated));
+router.get('/:account_id', at_least(SL.authenticated), its_mine, async (req, res) => {
+	const notification_list = await notifications.getNotifications(req.conn, req.params.account_id);
+	res.status(200).send(notification_list);
+});
+
+router.patch('/:account_id', at_least(SL.authenticated), its_mine, async (req, res) => {
+	// Next time we open the notifications, they'll all be read
+	await notifications.markAllNotificationsRead(req.conn, req.params.account_id);
+	res.status(204).send();
+});
+
+router.delete('/:account_id', at_least(SL.authenticated), its_mine, async (req, res) => {
+	const notification = await notifications.getNotificationById(req.conn, req.params.notification_id);
+
+	await notifications.dismissAllNotification(req.conn, req.account._id);
+	res.status(204).send();
+});
+
+router.get('/:account_id/:notification_id', at_least(SL.authenticated), its_mine, async (req, res) => {
+	const notification = await notifications.getNotificationById(req.conn, req.params.notification_id);
+	if (notification === null || !notification.account.equals(req.account._id)) {
+		res.status(404).send({error: 'Unknown notification'});
+		return;
+	}
+
+	res.status(200).send(notification);
+});
+
+router.patch('/:account_id/:notification_id', at_least(SL.authenticated), its_mine, async (req, res) => {
+	const notification = await notifications.getNotificationById(req.conn, req.params.notification_id);
+	if (notification === null || !notification.account.equals(req.account._id)) {
+		res.status(404).send({error: 'Unknown notification'});
+		return;
+	}
+
+	await notifications.markNotificationRead(req.conn, notification._id);
+
+	res.status(200).send(notification);
+});
+
+router.delete('/:account_id/:notification_id', at_least(SL.authenticated), its_mine, async (req, res) => {
+	const notification = await notifications.getNotificationById(req.conn, req.params.notification_id);
+	if (notification === null || !notification.account.equals(req.account._id)) {
+		res.status(404).send({error: 'Unknown notification'});
+		return;
+	}
+
+	await notifications.dismissNotification(req.conn, notification._id);
+	res.status(204).send();
+});
 
 export default router;
