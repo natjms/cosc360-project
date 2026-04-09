5198bbcf7be6d3a6bc8c76be33383539673c63e0
nat <natscott@nats.solutions>
2026-04-09 12:20:28 -0700

    feat(frontend): make SearchResults use search endpoint

--- frontend/src/SearchResults.jsx
+++ frontend/src/SearchResults.jsx
@@ -3,6 +3,39 @@ import { useState, useEffect } from 'react';
 
 import SearchBar from './components/SearchBar.jsx';
 
+const CatalogEntryResult = ({entry}) => {
+	return (
+		<article style={{ display: 'flex', gap: '20px', alignItems: 'start'}}>
+			{entry.cover && (
+				<img
+				src={`data:image/jpeg;base64,${entry.cover}`}
+				alt={entry.title}
+				style={{width: '100px', borderRadius: '4px', boxShadow: '0 2px 5px rgba(0,0,0,0.2'}}
+				/>
+			)}
+			<div style={{ flex: 1 }}>
+		        <h2 style={{ margin: '0 0 5px 0' }}>{entry.title}</h2>
+		        <p style={{ margin: '0', color: '#555' }}><strong>Author:</strong> {entry.author}</p>
+		        <p style={{ fontSize: '0.9em', color: '#777' }}><strong>ISBN:</strong> {entry.isbn}</p>
+		        <p>{entry.description}</p>
+		        <button
+		            onClick={() => handleDelete(entry._id)}
+		            style={{
+		                backgroundColor: '#dc3545',
+		                color: 'white',
+		                border: 'none',
+		                padding: '8px 12px',
+		                borderRadius: '4px',
+		                cursor: 'pointer'
+		            }}
+		        >
+		            Delete from Catalog
+		        </button>
+		    </div>
+		</article>
+	);
+};
+
 export default function SearchResults() {
 	const [search_params, setSearchParams] = useSearchParams();
 	const [search_results, setSearchResults] = useState(null);
@@ -11,7 +44,7 @@ export default function SearchResults() {
 
 	const refreshSearch = () => {
         const query = search_params.get('q') || '';
-        fetch(`/api/books/search?q=${encodeURIComponent(query)}`)
+        fetch(`/api/search/?q=${encodeURIComponent(query)}`)
             .then(res => res.json())
             .then(data => {
                 console.log("Books found:", data);
@@ -51,57 +84,32 @@ export default function SearchResults() {
 	};
 
 	return (
-	<main style={{ padding: '20px'}}>
-		<SearchBar
-			value={newQuery}
-			onChange={changeHandler}
-			onSubmit={submitHandler}
-			/>
+   	    <main style={{ padding: '20px'}}>
+  	        <SearchBar
+                value={newQuery}
+                onChange={changeHandler}
+                onSubmit={submitHandler} />
 			<hr style={{ margin: '20px 0'}}/>
-		{ search_results !== null ? (
-			search_results.length > 0 ? (
-				<ul style={{ listStyle: 'none', padding: 0}}>
-					{
-						search_results.map((book) => (
-							<li key={book._id} style={{ marginBottom: '30px', borderBottom: '1px solid #eee', paddingBottom: '20px'}}>
-								<article style={{ display: 'flex', gap: '20px', alignItems: 'start'}}>
-									{book.cover && (
-										<img 
-										src={`data:image/jpeg;base64,${book.cover}`}
-										alt={book.title}
-										style={{width: '100px', borderRadius: '4px', boxShadow: '0 2px 5px rgba(0,0,0,0.2'}}
-										/>
-									)}
-									<div style={{ flex: 1 }}>
-                                        <h2 style={{ margin: '0 0 5px 0' }}>{book.title}</h2>
-                                        <p style={{ margin: '0', color: '#555' }}><strong>Author:</strong> {book.author}</p>
-                                        <p style={{ fontSize: '0.9em', color: '#777' }}><strong>ISBN:</strong> {book.isbn}</p>
-                                        <p>{book.description}</p>
-                                        
-                                        <button 
-                                            onClick={() => handleDelete(book._id)}
-                                            style={{ 
-                                                backgroundColor: '#dc3545', 
-                                                color: 'white', 
-                                                border: 'none', 
-                                                padding: '8px 12px', 
-                                                borderRadius: '4px', 
-                                                cursor: 'pointer' 
-                                            }}
-                                        >
-                                            Delete from Catalog
-                                        </button>
-                                    </div>
-                                </article>
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No books found matching your search.</p>
-                )
-            ) : (
-                <p>Loading results...</p>
-            )}
+            { search_results !== null ?
+                <>
+                    { search_results.catalog.length > 0 ?
+                        <ul style={{ listStyle: 'none', padding: 0}}>
+                            { search_results.catalog.map((e) => (
+                                   <li key={e._id}
+                                    style={{
+                                        marginBottom: '30px',
+                                        borderBottom: '1px solid #eee',
+                                        paddingBottom: '20px'
+                                    }}>
+                                    <CatalogEntryResult entry={e} />
+                                </li>
+                            ))}
+                        </ul>
+                        : <p>No books found</p>
+                    }
+                </>
+                : <p>Loading results...</p>
+            }
         </main>
     );
 }
