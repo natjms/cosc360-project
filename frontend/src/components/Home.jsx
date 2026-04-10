538c49b53b6dea38357e5db1b21df353435c08a8
nat <natscott@nats.solutions>
2026-04-10 12:37:29 -0700

    feat(frontend): put random books in staff picks section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,53 +1,74 @@
- import './Home.css'
- import bookstore from "../assets/cozy-bookstore.jpg"
- import library from "../assets/libraryBox.jpg"
- import sevenHusbands from "../assets/sevenHusbands.jpg"
- import hungerGames from "../assets/hungerGames.jpg"
- import twilight from "../assets/twilight.jpg"
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function Home() { 
+import './Home.css'
+import bookstore from "../assets/cozy-bookstore.jpg"
+import library from "../assets/libraryBox.jpg"
+import sevenHusbands from "../assets/sevenHusbands.jpg"
+import hungerGames from "../assets/hungerGames.jpg"
+import twilight from "../assets/twilight.jpg"
+
+function Home() {
+    const [staff_picks, setStaffPicks] = useState([]);
+    const [errorMessage, setErrorMessage]= useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const random_books_response = await fetch('/api/books/random?count=3', {
+                headers: {'Content-Type': 'application/json'},
+            });
+            console.log(random_books_response);
+            if (!random_books_response.ok) {
+                try {
+                    const error_response = await random_books_response.json();
+                    console.error(error_response);
+                    alert(error_response.error);
+                    return;
+                } catch(e) {
+                    console.error(e);
+                    alert('An unknown error occured while retrieving books. Please try again later');
+                    return;
+                }
+            }
+
+            setStaffPicks(await random_books_response.json());
+        })();
+    }, []);
 
     return (
         <>
-        <div className = "home1">
-       
-        <div style = {{backgroundColor: "#B45253",
-            width: "60%", 
-            height: "920px"
-        }}>
+            { errorMessage &&
+                <p style={{ color: 'red' }}>{errorMessage}</p>
+            }
+            <div className = "home1">
+                <div style = {{backgroundColor: "#B45253",
+                    width: "60%",
+                }}>
+                    <h2 className = "stafftitle">Staff Picks</h2>
+                    <ul className = "staffpicks">
+                        { staff_picks.map((b, i) =>
+                            <li key={i} class='home-page-staff-pick'>
+                                <img src={b.cover} alt={`Cover of ${b.title}`}/>
+                                <div>
+                                    <p>
+                                        <em>{b.title}</em> — {b.description};
+                                    </p>
+                                    <p><Link to={`/catalog/${b.isbn}`}>Read more</Link></p>
+                                </div>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <img src={bookstore} alt="Bookstore" className="bookstore" style = {{width: "40%"}}/>
+            </div>
 
-            <h1 className = "stafftitle">STAFF PICKS</h1>
-            <ul className = "staffpicks">
-                <hr></hr>
-                <li>HARRY POTTER</li>
-                <hr></hr>
-                <li>A COURT OF THORNS AND ROSES</li>
-                <hr></hr>
-                <li>THE GREAT GATSBY</li>
-                <hr></hr>
-                <li>THE ATLAS SIX</li>
-                <hr></hr>
-                <li>THE FOUR WINDS</li>
-                <hr></hr>
-                <li>THE CAT WHO SAVED BOOKS</li>
-                <hr></hr>
-                <li>THE FIVE PEOPLE YOU MEET IN PERSON</li>
-                <hr></hr>
-                <li>THE HUNGER GAMES</li>
-                <hr></hr>
-            </ul>
-        </div>
-            <img src={bookstore} alt="Bookstore" className="bookstore" style = {{width: "40%"}}/>
-        </div>
-
-       <h2 style = {{margin: "40px", textAlign: "center"}}> A GATEWAY TO A NEW WORLD, STORIES, AND LIMITLESS IMAGINATION </h2>
+            <h2 style = {{margin: "40px", textAlign: "center"}}>
+                A GATEWAY TO A NEW WORLD, STORIES, AND LIMITLESS IMAGINATION
+            </h2>
 
        <img src = {library} alt = "Library" className = "libraryBox" style = {{width: "100%"}}/>
 
-
-
        <div className = "featuredBooks">
-        
         <div className = "book1" >
                 <img src={sevenHusbands} alt="sevenHusbands" className="sevenHusbands"/>
                 <p className = "books1">Aging and reclusive Hollywood movie icon Evelyn Hugo is finally 
