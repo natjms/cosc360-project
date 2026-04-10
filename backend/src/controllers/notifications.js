2c4dffbf56830cdac7fe07836b20b0ef15a40999
nat <natscott@nats.solutions>
2026-04-09 21:17:36 -0700

    feat(backend): add DAL to check if there's unread notifications

--- backend/src/controllers/notifications.js
+++ backend/src/controllers/notifications.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { SL, at_least } from '#src/authentication.js';
-import { connect_db } from '#src/db/connection.js';
+import { connect_db, objectId } from '#src/db/connection.js';
+import * as notifications from '#src/db/notifications.js';
 
 const router = express.Router();
 
@@ -23,6 +24,13 @@ router.use(connect_db);
 router.get('/:account_id', at_least(SL.authenticated), its_mine, async (req, res) => {
 	const notification_list = await notifications.getNotifications(req.conn, req.params.account_id);
 	res.status(200).send(notification_list);
+});
+
+router.get('/:account_id/unread', at_least(SL.authenticated), its_mine, async (req, res) => {
+	const unread =
+		await notifications.hasUnreadNotifications(req.conn, req.params.account_id);
+
+	res.status(200).send({ unread });
 });
 
 router.patch('/:account_id', at_least(SL.authenticated), its_mine, async (req, res) => {
