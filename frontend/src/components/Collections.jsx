fb19f9d39c07689b02be5b73b199ad20e1ce8e95
nat <natscott@nats.solutions>
2026-04-11 21:13:22 -0700

    fix(frontend): get collections pages working

--- frontend/src/components/Collections.jsx
+++ frontend/src/components/Collections.jsx
@@ -4,73 +4,44 @@ import logo from "../assets/logo.jpg";
 import './Collections.css'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import BookItem from './BookItem';
+import { Link } from 'react-router-dom'
 
 
 export default function Collections() {
-
     const [collections, setCollections] = useState([]); 
-
-    async function queryBook(id){
-	    try{
-		const res = await fetch(`api/books/${id}`);
-		const book = await res.json();
-		return book;
-	    }
-	    
-	    catch(err){
-		    console.log(err);
-	    }	
-    }
-
     async function queryData(){
 	    const res = await fetch('/api/collections/public');
 	    const coll = await res.json();
-
-	    const bookColl = await Promise.all(
-		coll.map((collection) => {
-			const book = collection.list?.map((id) => {return queryBook(id);});
-			return {...collection, book};
-		})
-	    );
-	console.log(bookColl);
-	setCollections(bookColl); 
-
+        setCollections(coll);
     }
-  useEffect(() => { 
-	queryData();
-  }, []);
-    const renderArr = collections.map( (collection) => {
-	console.log(collection);
 	
-	const books = collection.book.map(b => {
-		return <BookItem entry={b} key={b.isbn}/>;
-	});
+    useEffect(() => { 
+        queryData();
+    }, []);
 
-	return (
-		<div className='collection' id={collection._id}>
-			<h1>{collection.title}</h1> 
-			<p>{collection.description}</p>
-			<div className='books-container'>
-				{books}
-			</div>
-		</div>
+    return <>
+        <div className = "about about-banner">
+            <h1>All collections</h1>
+	        <button><h1><a className="lnk" href="/AddCollection">Add new collection</a></h1></button>
+	    </div>
 
-	);
-    });
-
-    return (
-      <>
-      <div className = "about about-banner">
-        <h1 >CONNECT, DISCOVER, IMAGINE</h1>
-	<div className='btnholder'>
-      </div>
-	         <button><h1><a className="lnk" href="/AddCollection">Add new collection</a></h1></button>
-	    
-	</div>
-	{renderArr}	
-      </>
-    );
+        <div className='collections-page-list-container'>
+            <ul>
+                { collections.length > 0 &&
+                    collections.map((collection, i) =>
+                        <li key={i}>
+                            <h2>
+                                <Link to={`/collection/${collection._id}`}>
+                                    {collection.title}
+                                </Link>
+                            </h2>
+                            <p>{collection.description}</p>
+                        </li>
+                    )
+                }
+            </ul>
+        </div>
+    </>;
 }
 
 
